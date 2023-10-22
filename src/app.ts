77c11aac424dae1f9cbb93ab71aa17569c6e9084
shahibur-shanto <shahibur407@gmail.com>
2023-10-23 00:16:43 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -11,13 +11,12 @@ const app: Application = express();
 app.use(cors());
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: 'GET,PUT,POST,DELETE',
+  credentials: true, // Enable credentials (cookies, HTTP authentication)
+}));
+
 
 //parser
 app.use(express.json());
