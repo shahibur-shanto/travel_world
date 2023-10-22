31baca0fb2d4ea72b17d913c10a7afd3257d5d98
shahibur-shanto <shahibur407@gmail.com>
2023-10-23 00:41:06 +0600

    cors again

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,15 @@ import routes from './app/routes';
 import cookieParser from 'cookie-parser';
 
 const app: Application = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+
 
 app.use(cors());
 app.use(cookieParser());
-
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: 'GET,PUT,POST,DELETE',
-  credentials: true, // Enable credentials (cookies, HTTP authentication)
-}));
-
 
 //parser
 app.use(express.json());
