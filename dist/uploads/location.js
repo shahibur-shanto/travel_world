fc2e114105556561206dc6fd57ea69968c98d99a
shahibur-shanto <shahibur407@gmail.com>
2023-10-28 21:18:03 +0600

    sdsd

---
+++ dist/uploads/location.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LOCATION = void 0;
+/* eslint-disable no-unused-vars */
+var LOCATION;
+(function (LOCATION) {
+    LOCATION["BALI"] = "bali";
+    LOCATION["SAJEK"] = "sajek";
+})(LOCATION = exports.LOCATION || (exports.LOCATION = {}));
