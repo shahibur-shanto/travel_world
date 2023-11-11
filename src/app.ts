a596901abc0d87898cf21440fee6a88f73077131
shahibur-shanto <shahibur407@gmail.com>
2023-11-11 16:28:46 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,16 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // app.use(cors());
 app.use(cookieParser());
 
