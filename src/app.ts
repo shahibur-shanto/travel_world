cdd116f740ec03595903a7acf2f6b0118c818b99
shahibur-shanto <shahibur407@gmail.com>
2023-11-11 18:37:07 +0600

    cors3

--- src/app.ts
+++ src/app.ts
@@ -6,31 +6,34 @@ import routes from './app/routes';
 import cookieParser from 'cookie-parser';
 
 const app: Application = express();
+import cors from 'cors';
 
-// app.use(
-//   cors({
-//     origin: '*',
-//     credentials: true,
-//   })
-// );
+app.use(cors({
+  origin: 'https://dancing-licorice-2ec233.netlify.app',
+  // You can also specify an array of allowed origins:
+  // origin: ['https://dancing-licorice-2ec233.netlify.app', 'https://another-allowed-origin.com'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // include credentials like cookies, authorization headers, etc.
+}));
 
-app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://dancing-licorice-2ec233.netlify.app'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  next();
-});
+
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     'Access-Control-Allow-Origin',
+//     'https://dancing-licorice-2ec233.netlify.app'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PUT, PATCH, DELETE'
+//   );
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//   );
+//   next();
+// });
 
 // app.use(cors());
 app.use(cookieParser());
